850af03add17c756ff71c832ae0c0ca27be6c4dc
KenA3 <90624279+KenAbraham18@users.noreply.github.com>
2023-01-10 15:37:10 +0000

    Functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("red").addEventListener("mouseover", showRed);
+function showRed() {
+document.getElementById("product-photo").src = "Porsche 911 Red.png";
+
+}
+
+
+
+document.getElementById("blue").addEventListener("mouseover", showBlue);
+function showBlue() {
+document.getElementById("product-photo").src = "Porsche 911 Blue.png";
+}
+
+document.getElementById("red").addEventListener("mouseover", showRED);
+function showRed() {
+document.getElementById("product-photo").src = "Porsche 911 Red.png";
+}
+
+
+
+document.getElementById("black").addEventListener("mouseover", showBLACK);
+function showBeige() {
+document.getElementById("product-photo").src = "Porche 911 Grey.png";
+}
